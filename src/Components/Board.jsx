f1b7e983b37fa49784e1c0e7374d15eb84acef10
simple-sifu <kattyntommy@gmail.com>
2023-01-02 02:21:40 -0500

    refactor

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import Square from './Square'
+import "./Board.styles.css"
 
 class Board extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            squares: Array(9).fill("-")
+            squares: Array(9).fill("---"),
+            xIsNext: true,
         }
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : '0';
+        this.setState(
+            {
+             squares: squares,
+             xIsNext: !this.state.xIsNext,
+            }
+        );
     }
 
     renderSquare(i){
@@ -24,8 +31,10 @@ class Board extends Component {
         );
     }
     render(){
+        const status = `Next player: ${this.state.xIsNext? 'X' : '0'}`;
         return (
             <div>
+                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
