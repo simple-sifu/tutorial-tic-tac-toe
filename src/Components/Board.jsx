547dc2ecf3429b2b2aad3f64d8530d64337d447b
simple-sifu <kattyntommy@gmail.com>
2023-01-02 03:05:54 -0500

    refactor

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -11,8 +11,29 @@ class Board extends Component {
         }
     }
 
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 4, 6],
+            [2, 5, 8],
+        ]
+        for (let i=0; i < lines.length; i++ ){
+            const [a,b,c] = lines[i];
+            if (squares[a] !== "---" && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (this.calculateWinner(squares) || squares[i] !== "---") return;
         squares[i] = this.state.xIsNext ? 'X' : '0';
         this.setState(
             {
@@ -30,8 +51,15 @@ class Board extends Component {
             />
         );
     }
+
     render(){
-        const status = `Next player: ${this.state.xIsNext? 'X' : '0'}`;
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = `Winner:  ${winner}`; 
+        }else {
+            status = `Next player: ${this.state.xIsNext? 'X' : '0'}`;
+        }
         return (
             <div>
                 <div className="status">{status}</div>
@@ -54,7 +82,6 @@ class Board extends Component {
         )
     }
 }
-
 
 
 export default Board;
